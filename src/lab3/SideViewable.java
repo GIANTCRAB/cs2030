d29452d550de9a86b41dba5d019a9bbc78df3b57
Huiren Woo <giantcrabby@gmail.com>
2019-09-12 23:09:29 +0800

    Add sideviewable interface

---
+++ src/lab3/SideViewable.java
@@ -0,0 +1,15 @@
+package lab3;
+
+public interface SideViewable {
+    Rubik rightView();
+
+    Rubik leftView();
+
+    Rubik upView();
+
+    Rubik downView();
+
+    Rubik backView();
+
+    Rubik frontView();
+}
