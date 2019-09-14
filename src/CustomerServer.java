b8d3e94aa7e057912f74e20363fb9775def74477
Huiren Woo <giantcrabby@gmail.com>
2019-09-14 15:58:21 +0800

    Add customer server

---
+++ src/CustomerServer.java
@@ -0,0 +1,47 @@
+public class CustomerServer implements Comparable<CustomerServer> {
+    private final Integer id;
+    private final Customer currentlyServing;
+
+    public CustomerServer(int id, Customer currentlyServing) {
+        this.id = id;
+        this.currentlyServing = currentlyServing;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public int getIntId() {
+        return id;
+    }
+
+    public double getAvailableTime() {
+        return this.getCurrentlyServing().getArrivalTime() + this.getCurrentlyServing().getServiceTime();
+    }
+
+    public boolean canServe(Customer otherCustomer) {
+        return this.getCurrentlyServing() == null || this.getAvailableTime() >= otherCustomer.getArrivalTime();
+    }
+
+    public CustomerServer serve(Customer customer) {
+        if (this.canServe(customer)) {
+            return new CustomerServer(this.getIntId(), customer);
+        }
+
+        return null;
+    }
+
+    private Customer getCurrentlyServing() {
+        return currentlyServing;
+    }
+
+    @Override
+    public int compareTo(CustomerServer customerServer) {
+        return this.getId().compareTo(customerServer.getId());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer served; next service @ %.3f", this.getAvailableTime());
+    }
+}
