105d1394d93d1cd2259bc7e146479004695e78ff
Huiren Woo <giantcrabby@gmail.com>
2019-09-17 22:34:21 +0800

    Change to use Sets

--- src/mockprac1/Menu.java
+++ src/mockprac1/Menu.java
@@ -1,18 +1,18 @@
 package mockprac1;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Menu {
     private List<MenuItem> menuItems = new ArrayList<>();
-    private List<String> categories = new ArrayList<>();
+    private Set<String> categories = new LinkedHashSet<>();
 
     public MenuItem add(String category, String itemName, int cost) {
         final MenuItem food = new Food(this.menuItems.size(), category, itemName, cost);
         this.menuItems.add(food);
-        if (!this.categories.contains(category)) {
-            this.categories.add(category);
-        }
+        this.categories.add(category);
         return food;
     }
 
@@ -21,9 +21,7 @@ public class Menu {
         for (Integer itemId : itemIdList) {
             comboItem.addOtherItem(this.menuItems.get(itemId));
         }
-        if (!this.categories.contains(category)) {
-            this.categories.add(category);
-        }
+        this.categories.add(category);
         this.menuItems.add(comboItem);
         return comboItem;
     }
