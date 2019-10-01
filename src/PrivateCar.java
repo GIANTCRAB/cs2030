407be61f4bf9e41757cf1b853a6d5c1b9e5b8ba1
Huiren Woo <giantcrabby@gmail.com>
2019-10-01 23:22:24 +0800

    Fix available services provided

--- src/PrivateCar.java
+++ src/PrivateCar.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 class PrivateCar extends Cab {
-    private static final Collection<Class<? extends Ride>> servicesProvided = Arrays.asList(JustRide.class, TakeACab.class);
+    private static final Collection<Class<? extends Ride>> servicesProvided = Arrays.asList(JustRide.class, ShareARide.class);
 
     PrivateCar(String carPlateNumber, int minutesAway) {
         super(carPlateNumber, minutesAway, PrivateCar.servicesProvided);
