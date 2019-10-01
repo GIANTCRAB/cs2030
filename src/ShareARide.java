3e33a485776a7ca308899a7506fbe51c8371a15d
Huiren Woo <giantcrabby@gmail.com>
2019-10-01 22:11:42 +0800

    Add share a ride

---
+++ src/ShareARide.java
@@ -0,0 +1,8 @@
+class ShareARide extends Ride {
+    ShareARide() {
+        super();
+        this.surchargeCost = 500;
+        this.distanceCost = 50;
+        this.costSharedWithPassengers = true;
+    }
+}
