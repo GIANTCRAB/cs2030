f52a192265f7d2d3fe041d7895259dccee9bef83
Huiren Woo <giantcrabby@gmail.com>
2019-10-07 15:37:37 +0800

    Remove optional and add level 2

--- src/Trace.java
+++ src/Trace.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class Trace<T> {
     private final T currentValue;
-    private final Optional<List<T>> traceHistory;
+    private final List<T> traceHistory;
 
     public Trace() {
-        this(null, Optional.empty());
+        this(null, null);
     }
 
-    public Trace(T currentValue, Optional<List<T>> traceHistory) {
+    public Trace(T currentValue, List<T> traceHistory) {
         this.currentValue = currentValue;
         this.traceHistory = traceHistory;
     }
@@ -16,7 +16,7 @@ public class Trace<T> {
     @SafeVarargs
     public static <T> Trace<T> of(T currentValue, T... traceValues) {
         final List<T> traceHistory = new ArrayList<>(Arrays.asList(traceValues));
-        return new Trace<>(currentValue, Optional.of(traceHistory));
+        return new Trace<>(currentValue, traceHistory);
     }
 
     public T get() {
@@ -24,16 +24,19 @@ public class Trace<T> {
     }
 
     public List<T> history() {
-        final Optional<List<T>> traceHistory = this.getTraceHistory();
-        if (traceHistory.isPresent()) {
-            final List<T> traceList = traceHistory.get();
-            traceList.add(this.get());
-            return traceList;
-        }
-        return null;
+        return this.traceHistory;
     }
 
-    public Optional<List<T>> getTraceHistory() {
-        return this.traceHistory;
+    public Trace<T> back(int steps) {
+        final List<T> history = this.history();
+        final int newSize = history.size() - steps - 1;
+        final List<T> newHistory = history.subList(0, newSize);
+        final T newCurrentValue = history.get(newSize);
+
+        return new Trace<>(newCurrentValue, newHistory);
+    }
+
+    public boolean equals(Trace<T> trace) {
+        return this.get().equals(trace.get()) && this.history().equals(trace.history());
     }
 }
