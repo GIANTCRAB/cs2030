e7fcc50771266d186aa8c4c8db8119ef03bd160a
Huiren Woo <giantcrabby@gmail.com>
2019-11-04 08:28:50 +0800

    Change matrix multiplication to parallel

--- src/lab9/MatrixMultiplication.java
+++ src/lab9/MatrixMultiplication.java
@@ -1,5 +1,8 @@
 package lab9;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 class MatrixMultiplication extends RecursiveTask<Matrix> {
@@ -71,6 +74,92 @@ class MatrixMultiplication extends RecursiveTask<Matrix> {
 
     @Override
     public Matrix compute() {
-        return Matrix.recursiveMultiply(this.m1, this.m2, this.m1Row, this.m1Col, this.m2Row, this.m2Col, this.dimension);
+        if (this.dimension <= MatrixMultiplication.FORK_THRESHOLD) {
+            return Matrix.nonRecursiveMultiply(this.m1, this.m2, this.m1Row, this.m1Col, this.m2Row, this.m2Col, this.dimension);
+        }
+
+        int size = dimension / 2;
+        Matrix result = new Matrix(dimension);
+        final List<MatrixMultiplication> subtasks = new CopyOnWriteArrayList<>();
+        final List<MatrixMultiplication> subtasks2 = new CopyOnWriteArrayList<>();
+        final List<MatrixMultiplication> subtasks3 = new CopyOnWriteArrayList<>();
+        final List<MatrixMultiplication> subtasks4 = new CopyOnWriteArrayList<>();
+        final MatrixMultiplication a11b11Thread = new MatrixMultiplication(m1, m2, m1Row, m1Col, m2Row, m2Col, size);
+        final MatrixMultiplication a12b21Thread = new MatrixMultiplication(m1, m2, m1Row, m1Col + size, m2Row + size, m2Col, size);
+        subtasks.add(a11b11Thread);
+        subtasks.add(a12b21Thread);
+        final MatrixMultiplication a11b12Thread = new MatrixMultiplication(m1, m2, m1Row, m1Col, m2Row, m2Col + size, size);
+        final MatrixMultiplication a12b12Thread = new MatrixMultiplication(m1, m2, m1Row, m1Col + size, m2Row + size, m2Col + size, size);
+        subtasks2.add(a11b12Thread);
+        subtasks2.add(a12b12Thread);
+        final MatrixMultiplication a21b11Thread = new MatrixMultiplication(m1, m2, m1Row + size, m1Col, m2Row, m2Col, size);
+        final MatrixMultiplication a21b21Thread = new MatrixMultiplication(m1, m2, m1Row + size, m1Col + size, m2Row + size, m2Col, size);
+        subtasks3.add(a21b11Thread);
+        subtasks3.add(a21b21Thread);
+        final MatrixMultiplication a21b12Thread = new MatrixMultiplication(m1, m2, m1Row + size, m1Col, m2Row, m2Col + size, size);
+        final MatrixMultiplication a22b22Thread = new MatrixMultiplication(m1, m2, m1Row + size, m1Col + size, m2Row + size, m2Col + size, size);
+        subtasks4.add(a21b12Thread);
+        subtasks4.add(a22b22Thread);
+
+        ForkJoinTask.invokeAll(subtasks)
+                .parallelStream()
+                .map(ForkJoinTask::join)
+                .reduce((matrix, matrix2) -> {
+                    for (int i = 0; i < size; i++) {
+                        final double[] m1m = matrix.m[i];
+                        final double[] m2m = matrix2.m[i];
+                        for (int j = 0; j < size; j++) {
+                            result.m[i][j] = m1m[j] + m2m[j];
+                        }
+                    }
+
+                    return result;
+                });
+
+        ForkJoinTask.invokeAll(subtasks2)
+                .parallelStream()
+                .map(ForkJoinTask::join)
+                .reduce((matrix, matrix2) -> {
+                    for (int i = 0; i < size; i++) {
+                        final double[] m1m = matrix.m[i];
+                        final double[] m2m = matrix2.m[i];
+                        for (int j = 0; j < size; j++) {
+                            result.m[i][j + size] = m1m[j] + m2m[j];
+                        }
+                    }
+
+                    return result;
+                });
+
+        ForkJoinTask.invokeAll(subtasks3)
+                .parallelStream()
+                .map(ForkJoinTask::join)
+                .reduce((matrix, matrix2) -> {
+                    for (int i = 0; i < size; i++) {
+                        final double[] m1m = matrix.m[i];
+                        final double[] m2m = matrix2.m[i];
+                        for (int j = 0; j < size; j++) {
+                            result.m[i + size][j] = m1m[j] + m2m[j];
+                        }
+                    }
+
+                    return result;
+                });
+
+        ForkJoinTask.invokeAll(subtasks4)
+                .parallelStream()
+                .map(ForkJoinTask::join)
+                .reduce((matrix, matrix2) -> {
+                    for (int i = 0; i < size; i++) {
+                        final double[] m1m = matrix.m[i];
+                        final double[] m2m = matrix2.m[i];
+                        for (int j = 0; j < size; j++) {
+                            result.m[i + size][j + size] = m1m[j] + m2m[j];
+                        }
+                    }
+
+                    return result;
+                });
+        return result;
     }
 }
