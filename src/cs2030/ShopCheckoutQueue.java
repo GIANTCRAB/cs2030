a17ec8736f65f083aa3248b9820fc42d62f196ba
Huiren Woo <giantcrabby@gmail.com>
2019-11-15 14:56:16 +0800

    Add methods for abstract class

--- src/cs2030/ShopCheckoutQueue.java
+++ src/cs2030/ShopCheckoutQueue.java
@@ -1,4 +1,40 @@
 package cs2030;
 
-public interface ShopCheckoutQueue {
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+abstract class ShopCheckoutQueue {
+    private final Queue<Customer> customerQueue;
+    private final int maxQueueCapacity;
+
+    ShopCheckoutQueue(int maxQueueCapacity) {
+        this.customerQueue = new ArrayDeque<>();
+        this.maxQueueCapacity = maxQueueCapacity;
+    }
+
+    public int getMaxQueueCapacity() {
+        return this.maxQueueCapacity;
+    }
+
+    public boolean canJoinCustomerQueue() {
+        return this.customerQueue.size() < this.getMaxQueueCapacity();
+    }
+
+    public ShopCheckoutQueue joinCustomerQueue(Customer customer) {
+        final Queue<Customer> customerQueue = this.getCustomerQueue();
+
+        if (this.canJoinCustomerQueue()) {
+            customerQueue.add(customer);
+        }
+
+        return this;
+    }
+
+    public Customer pollCustomer() {
+        return this.getCustomerQueue().poll();
+    }
+
+    private Queue<Customer> getCustomerQueue() {
+        return this.customerQueue;
+    }
 }
