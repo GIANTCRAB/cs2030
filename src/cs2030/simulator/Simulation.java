bb192f76315e8611b2123cf1829b74a98952f402
Huiren Woo <giantcrabby@gmail.com>
2019-11-15 14:02:44 +0800

    Add initial simulation files

---
+++ src/cs2030/simulator/Simulation.java
@@ -0,0 +1,14 @@
+package cs2030.simulator;
+
+/**
+ * The cs2030.simulator.Simulation class encapsulates information pertaining to our
+ * cs2030.simulator.Simulation.
+ *
+ * @author atharvjoshi
+ * @author weitsang
+ * @version CS2030 AY19/20 Sem 1 Lab 7
+ */
+public class Simulation {
+  /** The time a server takes to serve a customer. */
+  static final double SERVICE_TIME = 1.0;
+}
