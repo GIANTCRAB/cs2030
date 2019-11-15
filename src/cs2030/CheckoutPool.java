7e9fa3198e92f1ab728b2549705d5a1f8514765d
Huiren Woo <giantcrabby@gmail.com>
2019-11-15 15:24:40 +0800

    Add addCheckoutHandler

--- src/cs2030/CheckoutPool.java
+++ src/cs2030/CheckoutPool.java
@@ -3,6 +3,8 @@ package cs2030;
 public interface CheckoutPool {
     ShopCheckoutQueue getCheckoutQueue();
 
+    void addCheckoutHandler(CheckoutHandler checkoutHandler);
+
     CheckoutHandler[] getCheckoutHandler();
 
     CheckoutHandler getFirstCheckoutHandler();
