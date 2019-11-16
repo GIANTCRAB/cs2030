5e98c494f5aae732ce33949576a5ff435d98e960
Huiren Woo <giantcrabby@gmail.com>
2019-11-16 22:02:37 +0800

    Change some init part of Shop

--- src/cs2030/Shop.java
+++ src/cs2030/Shop.java
@@ -1,5 +1,6 @@
 package cs2030;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -15,56 +16,64 @@ import java.util.Optional;
  * @version CS2030 AY19/20 Sem 1 Lab 7
  */
 class Shop {
-  /** List of servers. */
-  private final List<Server> servers;
+    /**
+     * List of servers.
+     */
+    private final List<CheckoutCounter> checkoutCounters = new ArrayList<>();
 
-  /**
-   * Create a new shop with a given number of servers.
-   * @param numOfServers The number of servers.
-   */
-  Shop(int numOfServers) {
-    this.servers = Stream.iterate(1, i -> i + 1)
-      .map(i -> new Server(i))
-      .limit(numOfServers)
-      .collect(Collectors.toList());
-  }
+    /**
+     * Create a new shop with a given number of servers.
+     *
+     * @param numOfServers The number of servers.
+     */
+    Shop(int numOfServers, int numOfSelfCheckout) {
+        final int MAX_WAITING_CUSTOMERS = 1;
 
-  /**
-   * Constructor for updated shop.
-   */
-  Shop(List<Server> servers) {
-    this.servers = servers;
-  }
+        Stream.iterate(1, i -> i + 1)
+                .map(num -> new ServerCounter(new ServerCheckoutQueue(MAX_WAITING_CUSTOMERS), new Server(num)))
+                .limit(numOfServers)
+                .forEach(this.checkoutCounters::add);
 
-  /**
-   * Find a server matching the predicate.
-   *
-   * @param predicate A predicate to match the server with.
-   * @return Optional.empty if no server matches the predicate, or the
-   *     optional of the server if a matching server is found.
-   */
-  public Optional<Server> find(Predicate<Server> predicate) {
-    return this.servers.stream()
-        .filter(predicate)
-        .findFirst();
-  }
+        // TODO: initialize self checkout
+    }
 
-  /**
-   * Returns a new shop when one of the server changes its state.
-   */
-  public Shop replace(Server server) {
-    return new Shop(
-        servers.stream()
-            .map(s -> (s.equals(server) ? server : s))
-            .collect(Collectors.toList())
-            );
-  }
+    /**
+     * Constructor for updated shop.
+     */
+    Shop(List<Server> servers) {
+        this.servers = servers;
+    }
 
-  /**
-   * Return a string representation of this shop.
-   * @return A string reprensetation of this shop.
-   */
-  public String toString() {
-    return servers.toString();
-  }
+    /**
+     * Find a server matching the predicate.
+     *
+     * @param predicate A predicate to match the server with.
+     * @return Optional.empty if no server matches the predicate, or the
+     * optional of the server if a matching server is found.
+     */
+    public Optional<CheckoutCounter> find(Predicate<CheckoutCounter> predicate) {
+        return this.checkoutCounters.stream()
+                .filter(predicate)
+                .findFirst();
+    }
+
+    /**
+     * Returns a new shop when one of the server changes its state.
+     */
+    public Shop replace(Server server) {
+        return new Shop(
+                servers.stream()
+                        .map(s -> (s.equals(server) ? server : s))
+                        .collect(Collectors.toList())
+        );
+    }
+
+    /**
+     * Return a string representation of this shop.
+     *
+     * @return A string reprensetation of this shop.
+     */
+    public String toString() {
+        return this.checkoutCounters.toString();
+    }
 }
