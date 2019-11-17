620f0dbcf2af6e13f91968d6578c5186a99e6c53
Huiren Woo <giantcrabby@gmail.com>
2019-11-17 23:25:36 +0800

    Change return type to Optional<Event[]>

--- src/cs2030/HasRestState.java
+++ src/cs2030/HasRestState.java
@@ -1,9 +1,11 @@
 package cs2030;
 
+import java.util.Optional;
+
 public interface HasRestState {
     RestStates getRestState();
 
-    void takeRest();
+    Optional<Event[]> takeRest(double doneTime);
 
-    void stopRest();
+    Optional<Event[]> stopRest();
 }
